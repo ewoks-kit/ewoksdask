abc1a1a2d568553bc10eb5db9bf0e1e74224f39a
woutdenolf <woutdenolf@users.sf.net>
2021-07-05 18:13:56 +0200

    Rename esrfgraph to ewoksgraph

--- ewoksdask/bindings.py
+++ ewoksdask/bindings.py
@@ -13,34 +13,34 @@ from ewokscore import load_graph
 def execute_task(node_name, *inputs):
     node_name = node_name[:-3]
     info = inputs[0]
-    esrfgraph = load_graph(info["esrfgraph"])
-    task = esrfgraph.instantiate_task_static(node_name, varinfo=info["varinfo"])
+    ewoksgraph = load_graph(info["ewoksgraph"])
+    task = ewoksgraph.instantiate_task_static(node_name, varinfo=info["varinfo"])
     task.execute()
     return info
 
 
-def convert_graph(esrfgraph, varinfo):
+def convert_graph(ewoksgraph, varinfo):
     daskgraph = dict()
-    for target in esrfgraph.graph.nodes:
-        sources = tuple(source for source in esrfgraph.predecessors(target))
+    for target in ewoksgraph.graph.nodes:
+        sources = tuple(source for source in ewoksgraph.predecessors(target))
         if not sources:
-            sources = ({"esrfgraph": esrfgraph, "varinfo": varinfo},)
+            sources = ({"ewoksgraph": ewoksgraph, "varinfo": varinfo},)
         partial = (execute_task, target + "...")
         daskgraph[target] = partial + sources
     return daskgraph
 
 
 def job(graph, representation=None, varinfo=None, scheduler=None):
-    esrfgraph = load_graph(source=graph, representation=representation)
-    if esrfgraph.is_cyclic:
+    ewoksgraph = load_graph(source=graph, representation=representation)
+    if ewoksgraph.is_cyclic:
         raise RuntimeError("Dask can only execute DAGs")
-    if esrfgraph.has_conditional_links:
+    if ewoksgraph.has_conditional_links:
         raise RuntimeError("Dask cannot handle conditional links")
-    daskgraph = convert_graph(esrfgraph, varinfo)
+    daskgraph = convert_graph(ewoksgraph, varinfo)
 
     nodes = list()
-    for node in esrfgraph.graph.nodes:
-        if len(list(esrfgraph.graph.successors(node))) == 0:
+    for node in ewoksgraph.graph.nodes:
+        if len(list(ewoksgraph.graph.successors(node))) == 0:
             nodes.append(node)
 
     if scheduler is None:
