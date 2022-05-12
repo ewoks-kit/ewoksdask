4d4de92f937bb2f7745faea7891b99fb8d6bde0e
woutdenolf <woutdenolf@users.sf.net>
2022-05-12 13:30:17 +0200

    harmonize execute_graph output

--- ewoksdask/tests/test_examples.py
+++ ewoksdask/tests/test_examples.py
@@ -5,9 +5,7 @@ from ewokscore import load_graph
 
 from ewokscore.tests.examples.graphs import graph_names
 from ewokscore.tests.examples.graphs import get_graph
-from ewokscore.tests.utils.results import assert_execute_graph_all_tasks
-from ewokscore.tests.utils.results import assert_execute_graph_tasks
-from ewokscore.tests.utils.results import filter_expected_results
+from ewokscore.tests.utils.results import assert_execute_graph_default_result
 
 
 @pytest.mark.parametrize("graph_name", graph_names())
@@ -23,28 +21,6 @@ def test_examples(graph_name, tmpdir, scheduler, scheme):
     if ewoksgraph.is_cyclic or ewoksgraph.has_conditional_links:
         with pytest.raises(RuntimeError):
             execute_graph(graph, scheduler=scheduler, varinfo=varinfo)
-        return
-
-    result = execute_graph(
-        graph, scheduler=scheduler, varinfo=varinfo, results_of_all_nodes=True
-    )
-    assert_all_results(ewoksgraph, result, expected, varinfo)
-    result = execute_graph(
-        graph, scheduler=scheduler, varinfo=varinfo, results_of_all_nodes=False
-    )
-    assert_end_results(ewoksgraph, result, expected, varinfo)
-
-
-def assert_all_results(ewoksgraph, result, expected, varinfo):
-    if varinfo:
-        scheme = varinfo.get("scheme")
     else:
-        scheme = None
-    if scheme:
-        assert_execute_graph_all_tasks(ewoksgraph, expected, varinfo=varinfo)
-    assert_execute_graph_tasks(result, expected, varinfo=varinfo)
-
-
-def assert_end_results(ewoksgraph, result, expected, varinfo):
-    expected = filter_expected_results(ewoksgraph, expected, end_only=True)
-    assert_execute_graph_tasks(result, expected, varinfo=varinfo)
+        result = execute_graph(graph, scheduler=scheduler, varinfo=varinfo)
+        assert_execute_graph_default_result(ewoksgraph, result, expected, varinfo)
