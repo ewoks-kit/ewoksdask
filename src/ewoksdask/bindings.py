3bc8ae590c820eb590108a2fbfd4118f45561b70
woutdenolf <woutdenolf@users.sf.net>
2024-11-02 19:09:23 +0100

    add optional time profiling

--- src/ewoksdask/bindings.py
+++ src/ewoksdask/bindings.py
@@ -43,6 +43,7 @@ def _execute_task(execute_options, *inputs):
         inputs=dynamic_inputs,
         varinfo=execute_options.get("varinfo"),
         execinfo=execute_options.get("execinfo"),
+        profile_directory=execute_options.get("profile_directory"),
     )
 
     task.execute()
@@ -109,6 +110,7 @@ def _execute_graph(
     merge_outputs: Optional[bool] = True,
     varinfo: Optional[dict] = None,
     execinfo: Optional[dict] = None,
+    profile_directory: Optional[str] = None,
     scheduler: Union[dict, str, None, Client] = None,
     scheduler_options: Optional[dict] = None,
 ) -> Dict[NodeIdType, Any]:
@@ -118,7 +120,12 @@ def _execute_graph(
         if ewoksgraph.has_conditional_links:
             raise RuntimeError("Dask cannot handle conditional links")
 
-        daskgraph = _create_dask_graph(ewoksgraph, varinfo=varinfo, execinfo=execinfo)
+        daskgraph = _create_dask_graph(
+            ewoksgraph,
+            varinfo=varinfo,
+            execinfo=execinfo,
+            profile_directory=profile_directory,
+        )
         outputs = graph_io.parse_outputs(ewoksgraph.graph, outputs)
         node_ids = list(analysis.topological_sort(ewoksgraph.graph))
 
