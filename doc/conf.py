656710c931336c89ebf047779501b66fd8706d41
Loic Huder <loic.huder@esrf.fr>
2025-01-17 09:48:08 +0100

    Migrate to pyproject.toml

--- doc/conf.py
+++ doc/conf.py
@@ -4,7 +4,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-from ewoksdask import __version__ as release
+import importlib.metadata
+
+release = importlib.metadata.version("ewoksdask")
 
 project = "ewoksdask"
 version = ".".join(release.split(".")[:2])
